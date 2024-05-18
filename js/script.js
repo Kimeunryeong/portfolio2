8f0d3cf41c95ea31a0087eb5d2e8af686ef5449c
Kimeunryeong <150204506+Kimeunryeong@users.noreply.github.com>
2024-05-19 06:02:05 +0900

    ㅇ

--- js/script.js
+++ js/script.js
@@ -9,3 +9,45 @@ btn.addEventListener("click", () => {
     btn.innerHTML = "Menu";
   }
 });
+
+// 타이핑 효과
+let target = document.querySelector("#main-effect");
+
+function randomString() {
+  let stringArr = [
+    "Learn to HTML",
+    "Learn to CSS",
+    "Learn to Javascript",
+    "Learn to jQuery",
+    "Learn to PHP",
+    "Learn to React",
+  ];
+  let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+  let selectStringArr = selectString.split("");
+
+  return selectStringArr;
+}
+
+// 타이핑 리셋
+function resetTyping() {
+  target.textContent = "";
+  dynamic(randomString());
+}
+// 한글자씩 텍스트 출력 함수
+function dynamic(randomArr) {
+  if (randomArr.length > 0) {
+    target.textContent += randomArr.shift();
+    setTimeout(function () {
+      dynamic(randomArr);
+    }, 80);
+  } else {
+    setTimeout(resetTyping, 3000);
+  }
+}
+dynamic(randomString());
+
+// 깜빡임 효과
+function blink() {
+  target.classList.toggle("active");
+}
+setInterval(blink, 500);
